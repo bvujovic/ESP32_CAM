4a0ed5108850dfd883608deb1b64cd80ad078374
bvujovic <bv.net@outlook.com>
2020-11-23 16:35:11 +0100

    Project PreviewCam finished. Added: preview sd card images.

--- projects/PreviewCam/PreviewCam.cpp
+++ projects/PreviewCam/PreviewCam.cpp
@@ -10,7 +10,8 @@ Blinky blnk(33, false);
 
 #include <AiThinkerCam.h>
 AiThinkerCam cam;
-// camera_fb_t *fb = NULL;
+
+#include "SD_MMC.h" // SD Card ESP32
 
 void PreviewHandler()
 {
@@ -40,12 +41,47 @@ void PreviewHandler()
 
 void SettingsHandler()
 {
-    tprintln("settings");        
+    tprintln("settings");
     sensor_t *s = esp_camera_sensor_get();
     s->set_framesize(s, (framesize_t)server.arg("res").toInt());
     s->set_brightness(s, server.arg("bright").toInt());
     s->set_gainceiling(s, (gainceiling_t)server.arg("gain").toInt());
     SendEmptyText(server);
+}
+
+void ListSdCardHandler()
+{
+    String str;
+    File dir = SD_MMC.open("/");
+    if (dir)
+    {
+        File f;
+        while (f = dir.openNextFile())
+        {
+            if (strstr(f.name(), ".jpg")) // samo me slike interesuju
+                (str += f.name()) += "\n";
+            f.close();
+        }
+        dir.close();
+        server.send(200, "text/x-csv", str);
+    }
+    else
+        tprintln("dir root fail");
+}
+
+void SdCardImgHandler()
+{
+    File f = SD_MMC.open("/" + server.arg("img"), "r");
+    if (f)
+    {
+        server.streamFile(f, "image/jpeg");
+        f.close();
+    }
+    else
+    {
+        server.send_P(404, "text/plain", "Error reading file");
+        tprintln(server.arg("img") + " - error reading file.");
+    }
 }
 
 // Konektovanje na WiFi, postavljanje IP adrese i startovanje veb servera.
@@ -55,18 +91,15 @@ void WiFiOn()
     WiFi.mode(WIFI_STA);
     ConnectToWiFi();
     SetupIPAddress(60);
-    // server.on("/", []() { HandleDataFile(server, "/index.html", "text/html"); });
-    // server.on("/inc/vehicle.png", []() { HandleDataFile(server, "/inc/vehicle.png", "image/png"); });
-    // server.on("/inc/script.js", []() { HandleDataFile(server, "/inc/script.js", "text/javascript"); });
     // server.on("/text_editor.html", []() { HandleDataFile(server, "/text_editor.html", "text/html"); });
-    // application/json
-
     // void serveStatic(const char* uri, fs::FS& fs, const char* path, const char* cache_header = NULL );
     server.on("/", []() { HandleDataFile(server, "/index.html", "text/html"); });
-    server.on("/webcam.png", []() { HandleDataFile(server, "/webcam.png", "image/png"); });
+    server.on("/webcam.png", []() { delay(3000); HandleDataFile(server, "/webcam.png", "image/png"); });
     server.on("/preview", PreviewHandler);
     server.on("/settings", SettingsHandler);
     server.on("/txt", []() { server.send(200, "text/plain", "ESP is online..."); });
+    server.on("/listSdCard", ListSdCardHandler);
+    server.on("/sdCardImg", SdCardImgHandler);
 
     server.begin();
     tprintln("WiFi ON");
@@ -77,8 +110,11 @@ void setup()
     blnk.ledOn(true);
     tbegin(115200);
     SPIFFS.begin();
-
-    // ReadConfigIni();
+    if (!SD_MMC.begin("/sdcard", true)) // 1-bitni mod
+    {
+        tprintln("SD Card Mount Failed");
+        blnk.blink(250, 0);
+    }
 
     WiFiOn();
 
